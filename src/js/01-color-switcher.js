369a9db480051d0e4027d48382f0f57493a34e33
ArtemKotlar <artemkotlar93@gmail.com>
2022-12-29 16:09:40 +0200

    Завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+let interval = null;
+let isActive = false;
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', onStopColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  interval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.btnStart.disadled = true;
+}
+
+function onStopColor() {
+  clearInterval(interval);
+  refs.btnStop.disadled = false;
+  isActive = false;
+}
